1d39bced56543668e3db8f4c64f52b03014c53a6
Alex Rodrigues <alex.rodrigues.developer@gmail.com>
2018-11-21 16:49:48 -0200

    initial commit

---
+++ PlaygroundAnimation/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  PlaygroundAnimation
+//
+//  Created by Alex Rodrigues on 21/11/18.
+//  Copyright © 2018 Alex Rodrigues. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
